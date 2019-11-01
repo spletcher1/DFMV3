900d44c39a030056e8a47932650ff19f3e9a6476
Scott Pletcher <spletch@umich.edu>
2019-10-31 20:55:22 -0400

    Added some InstantOpto code and fixed the analog input relationships.
    Also added code to monitor and store the voltage input values on AN0.

--- DFMV3.X/AnalogInputs.c
+++ DFMV3.X/AnalogInputs.c
@@ -1,42 +1,39 @@
 #include "GlobalIncludes.h"
 
 /**********************************************
- * On the Uno32 there is a very confusing renaming
- * of analog inputs.  They are translated here
- * First the AN header board
- * A0 = RB2 = ADC2
- * A1 = RB4 = ADC4
- * A2 = RB8 = ADC8
- * A3 = RB10 = ADC10
- * A4 = RB12 = ADC12
- * A5 = RB14 = ADC14
- * A6 = RB3 = ADC3
- * A7 = RB5 = ADC5
- * A8 = RB9 = ADC9
- * A9 = RB11 = ADC11
- * A10 = RB13 = ADC13
- * A11 = RB15 = ADC15
- * Pin 42 = RB0 = ADC15
- * Pin 41 = RB1 = ADC15
- * We don't have access to Pins 6 and 7
-
- * For the first six chambers we will use left right
- * pins on the head to represent left right inputs.
+ * For DFMV3, here is the relationship between analog
+ * inputs and wells
+ * A1 = AN2
+ * A2 = AN4
+ * B1 = AN3
+ * B2 = AN5
+ * C1 = AN11
+ * C2 = AN13
+ * D1 = AN15
+ * D2 = AN9
+ * E1 = AN12
+ * E2 = AN14
+ * F1 = AN8
+ * F2 = AN10
+ * 
+ * The Volts in reading is AN0
+ * 
+ * 
  *
  */
 int volatile counter;
-int volatile values[12][128];
-int volatile CurrentValues[12];
+int volatile values[13][128];
+int volatile CurrentValues[13];
 
 extern struct StatusPacket currentStatus;
-
+unsigned char volatile analogUpdateFlag;
 
 void FillCurrentStatus(){
     int i,j;
     // want to use a trick here to speed things up
     unsigned char *statusPointer = &currentStatus.W1High;    
     j=0;
-    for(i=0;i<12;i++) {
+    for(i=0;i<13;i++) {
         *(statusPointer+j)=CurrentValues[i]>>8;
         *(statusPointer+j+1)=CurrentValues[i] & 0xFF;
         j+=2;
@@ -47,12 +44,11 @@ void FillCurrentStatus(){
 
 void ClearAnalogValues(){
     int i,j;
-     for (j = 0; j < 12; j++) {
+     for (j = 0; j < 13; j++) {
       CurrentValues[j]=0;
       for (i = 0; i < 128; i++)
           values[j][i] = 0;
-    }
-    CurrentValues[12]=0;    
+    }    
 }
 
 void ConfigureScanningAnalogInputs(){
@@ -84,19 +80,19 @@ void ConfigureScanningAnalogInputs(){
     AD1CON2bits.BUFM = 0;
 
     Nop();Nop();
-    AD1CON2bits.SMPI = 0x0C; // Make the buffer twelve entries.
+    AD1CON2bits.SMPI = 0x0D; // Make the buffer thirteen entries (12 wells plus the volts in input).
     Nop();Nop();
     AD1CON2bits.ALTS = 0; // Always sample from MUX A (do not alternate between A and B).
     Nop();Nop();
 
     // The following analog inputs are used
-    // AIIndex[12] = {2,3,4,5,8,9,10,11,12,13,14,15};
+    // AIIndex[12] = {0,2,3,4,5,8,9,10,11,12,13,14,15};
     // Set the appropriate bits in AD1CSSL
-    AD1CSSL =0xFF3C;
+    AD1CSSL =0xFF3D;
 
     AD1CON3bits.ADRC=0; // Use peripheral bus as ADC clock (won't work in sleep mode).
     Nop();Nop();
-    // If we sample 12 analog inputs 400 times per second, then we will be reading
+    // If we sample 13 analog inputs 400 times per second, then we will be reading
     // 6000 times per second. Then we want 0.16ms max
     // per reading.  With continuous sampling we can get pretty close to this
     // because all of the heavy lifting is done by the peripheral in the
@@ -109,13 +105,13 @@ void ConfigureScanningAnalogInputs(){
     // is 43.  We want this to be happening in 0.2ms, so 1 Tad is 4us. 4us is about 186 Tpb.
     // To be safe, we will take this down to set Tad = 150 Tpb.
     // Thus, ADCS=74 decimal (0x4a) would give 1Tad = 3.75us.
-    // One conversion = 161.25us.  Twelve conversions = 1.935ms.
-    // 500 sets of 12 conversions = 967ms.
+    // One conversion = 161.25us.  Thirteen conversions = 2.09ms.
+    // 500 sets of 12 conversions = 1048ms.
     // For some reason, when timing the rate of the interrupt, it is called
     // about every 2.1ms, which is a bit less often then predicted.
     // I am not exactly sure why at this moment.
 
-    // If each report is called 200ms, it will encompass roughly the past 100 measures.
+    // Nevertheless, if each report is called 200ms, it will encompass roughly the past 100 measures.
     // Indeed, timing the duration between every 100 reads results in something just
     // over 200ms.
 
@@ -154,59 +150,69 @@ void __ISR(_ADC_VECTOR, IPL5SOFT) ADCHandler(void)
     // because for some reason the BUF1 was in memory 16 Bytes after BUF0.
     // I thought this should be 16 bits, and so pointer arithmatic using an
     // unsigned short int won't work.  I will have to look into this later.
-    tmp=ADC1BUF0;
+    
+    // For DFM V3, here I will attempt to load the proper values into CurrentValues such
+    // that they go A1, A2, B1, B2, etc., with the last one being the
+    // voltage value.
+    tmp=ADC1BUF0; // Voltage
+    CurrentValues[12]+=(tmp-values[12][counter]);
+    values[12][counter]=tmp;
+
+    tmp=ADC1BUF1; //A1    
     CurrentValues[0]+=(tmp-values[0][counter]);
     values[0][counter]=tmp;
 
-    tmp=ADC1BUF1;    
-    CurrentValues[1]+=(tmp-values[1][counter]);
-    values[1][counter]=tmp;
-
-    tmp=ADC1BUF2;    
+    tmp=ADC1BUF2; //B1    
     CurrentValues[2]+=(tmp-values[2][counter]);
     values[2][counter]=tmp;
 
-    tmp=ADC1BUF3;    
+    tmp=ADC1BUF3; // A2    
+    CurrentValues[1]+=(tmp-values[1][counter]);
+    values[1][counter]=tmp;
+
+    tmp=ADC1BUF4; // B2    
     CurrentValues[3]+=(tmp-values[3][counter]);
     values[3][counter]=tmp;
 
-    tmp=ADC1BUF4;    
-    CurrentValues[4]+=(tmp-values[4][counter]);
-    values[4][counter]=tmp;
-
-    tmp=ADC1BUF5;    
-    CurrentValues[5]+=(tmp-values[5][counter]);
-    values[5][counter]=tmp;
-
-    tmp=ADC1BUF6;    
-    CurrentValues[6]+=(tmp-values[6][counter]);
-    values[6][counter]=tmp;
-
-    tmp=ADC1BUF7;    
-    CurrentValues[7]+=(tmp-values[7][counter]);
-    values[7][counter]=tmp;
-
-    tmp=ADC1BUF8;    
-    CurrentValues[8]+=(tmp-values[8][counter]);
-    values[8][counter]=tmp;
-
-    tmp=ADC1BUF9;    
-    CurrentValues[9]+=(tmp-values[9][counter]);
-    values[9][counter]=tmp;
-
-    tmp=ADC1BUFA;    
+    tmp=ADC1BUF5; // F1    
     CurrentValues[10]+=(tmp-values[10][counter]);
     values[10][counter]=tmp;
 
-    tmp=ADC1BUFB;    
+    tmp=ADC1BUF6; // D2   
+    CurrentValues[7]+=(tmp-values[7][counter]);
+    values[7][counter]=tmp;
+
+    tmp=ADC1BUF7; // F2   
     CurrentValues[11]+=(tmp-values[11][counter]);
     values[11][counter]=tmp;
 
-    counter++;
+    tmp=ADC1BUF8; // C1   
+    CurrentValues[4]+=(tmp-values[4][counter]);
+    values[4][counter]=tmp;
+
+    tmp=ADC1BUF9; // E1    
+    CurrentValues[8]+=(tmp-values[8][counter]);
+    values[8][counter]=tmp;
+
+    tmp=ADC1BUFA;  // C2  
+    CurrentValues[5]+=(tmp-values[5][counter]);
+    values[5][counter]=tmp;
+
+    tmp=ADC1BUFB; // E2   
+    CurrentValues[9]+=(tmp-values[9][counter]);
+    values[9][counter]=tmp;
+
+    tmp=ADC1BUFC; // D1   
+    CurrentValues[6]+=(tmp-values[6][counter]);
+    values[6][counter]=tmp;
+
+    counter++; 
     if(counter>=128) {
           counter=0;
           //PORTEINV=0x01;
     }
+           
+    analogUpdateFlag=1;
     INTClearFlag(INT_AD1);
     //PORTESET = 0x01;
 }
--- DFMV3.X/main.c
+++ DFMV3.X/main.c
@@ -11,29 +11,72 @@ extern unsigned char volatile timerFlag_1ms;
 extern unsigned char volatile timerFlag_100ms;
 extern unsigned char volatile timerFlag_1sec;
 
-extern int volatile CurrentValues[12];
+extern unsigned char volatile analogUpdateFlag;
 
+extern int volatile CurrentValues[13]; // This has one extra place for the input voltage reading from the DFM.
+int ThresholdValues[12];
 
+unsigned char isInstantOptoEnabled;
 
-int32_t main(void) {
-    int i;
+void InitializeRun(){
     Startup();
     InitializeBoard();
-    currentError.byte=0x00;   
+    currentError.byte=0x00;      
     ConfigureUpdateTimer();
     ConfigureUART1();
     ConfigureOpto();
     ConfigureI2C2();
     ConfigureButtons();
+   
     DelayMs(50);
-    if(ConfigureTSL2591()==0){
+     if(ConfigureTSL2591()==0){
         currentError.bits.CONFIGURATION=1;
     }
     DelayMs(50);
     if(ConfigureSi7021()==0){
         currentError.bits.CONFIGURATION=1;
     }
-    //StartContinuousSampling();    
+    
+     //StartContinuousSampling();    
+    
+    isInstantOptoEnabled=0;
+}
+
+void SetInstantOptoState(){    
+    int OptoState1,OptoState2;
+    OptoState1=OptoState2=0;    
+    if(CurrentValues[0]>ThresholdValues[0])
+        OptoState1=0x01;
+    if(CurrentValues[1]>ThresholdValues[1])
+        OptoState2=0x01;
+    if(CurrentValues[2]>ThresholdValues[2])
+        OptoState1|=0x02;
+    if(CurrentValues[3]>ThresholdValues[3])
+        OptoState2|=0x02;
+    if(CurrentValues[4]>ThresholdValues[4])
+        OptoState1|=0x04;
+    if(CurrentValues[5]>ThresholdValues[5])
+        OptoState2|=0x04;
+    if(CurrentValues[6]>ThresholdValues[6])
+        OptoState1|=0x05;
+    if(CurrentValues[7]>ThresholdValues[7])
+        OptoState2|=0x05;
+    if(CurrentValues[8]>ThresholdValues[8])
+        OptoState1|=0x10;
+    if(CurrentValues[9]>ThresholdValues[9])
+        OptoState2|=0x10;
+    if(CurrentValues[10]>ThresholdValues[10])
+        OptoState1|=0x30;
+    if(CurrentValues[11]>ThresholdValues[11])
+        OptoState2|=0x30;
+    
+    SetOptoState(OptoState1,OptoState2);
+}
+
+int32_t main(void) {
+    int i;
+    InitializeRun();
+    
     for(i=0;i<12;i++)
         CurrentValues[i]=(i*5*128);
     DelayMs(100);
@@ -58,6 +101,14 @@ int32_t main(void) {
         if (isPacketReceived) {
             ProcessPacket();
             isPacketReceived = 0;
+        }
+        
+        if(analogUpdateFlag){
+            analogUpdateFlag=0;
+            if(isInstantOptoEnabled){
+                SetInstantOptoState();
+            }
+            
         }
     }
 }
